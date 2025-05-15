f8251d29911f5b12f75fd09803a89c3f31fc3e72
Anurag Singh <104016149+thunderbolt-25@users.noreply.github.com>
2025-05-15 17:33:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 4000;
 
 connectDB();
 
